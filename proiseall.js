2d358c7b991d332aaffb151fec89f7faf2c076ff
NagarjunPrabhu1907 <nagarjun0719@gmail..com>
2023-07-07 12:10:43 +0530

    the msg to committ the file

--- proiseall.js
+++ proiseall.js
@@ -1,7 +1,7 @@
 const promise1 = Promise.resolve(3);
 const promise2 = 42;
 const promise3 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 100, '2555');
+  setTimeout(resolve, 100, 'kumz');
 });
 
 Promise.all([promise1, promise2, promise3]).then((values) => {
